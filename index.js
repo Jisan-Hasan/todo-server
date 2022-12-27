3c77aaf026b9d49da94ecf581f14c47843459d45
Jisan-Hasan <jisan.jh5@gmail.com>
2022-12-27 17:08:48 +0600

    basic server setup done

---
+++ index.js
@@ -0,0 +1,36 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+// define port
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+// database setup
+const uri = process.env.DB_URI;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
+
+async function run(){
+
+}
+run().catch(err => console.log(err));
+
+
+app.get("/", (req, res) => {
+    res.send("Server is running!");
+});
+
+// listen port
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+});
